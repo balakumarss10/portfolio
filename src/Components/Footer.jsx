e2e86fb34657bc2a1478c3dcd00c3ad5f3503464
Bala <balakumarss10@gmail.com>
2024-05-30 23:07:38 +0530

    Head section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,34 @@
-import Social from './Social'
+import Social from "./Social";
 
 const Footer = () => {
   const date = new Date().getFullYear();
 
   return (
     <>
-    <footer className="bg-[#20242F] py-5 inter text-white tracking-wide">
-    <div className="w-full md:w-4/5 flex flex-col md:flex-row justify-between py-3 px-3 mx-auto">
-       <div className="py-2 w-full sm:w-3/5 md:w-1/3">
-        <h1 className="text-xl sora font-bold uppercase">Balakumar</h1>
-        <p className="py-2 text-sm leading-loose">A Frontend Developer creating website and web app interfaces that drive product success.</p> 
-       </div>
-       <div className="py-2">
-        <h1 className="sora font-bold text-xl pb-3 uppercase">Social</h1>
-        <p className='flex pb-2'><Social /></p>
-        <a className='text-blue-400' href='mailto:balakumarss10@gmail.com'>balakumarss@gmail.com</a>
-        </div>            
-    </div>
+      <footer className="bg-[#20242F] py-5 inter text-white tracking-wide">
+        <div className="w-full md:w-4/5 flex flex-col md:flex-row justify-between py-3 px-3 mx-auto">
+          <div className="py-2 w-full sm:w-3/5 md:w-1/3">
+            <h1 className="text-xl sora font-bold uppercase">Balakumar</h1>
+            <p className="py-2 text-sm leading-loose">
+              A Frontend Developer creating website and web app interfaces that
+              drive product success.
+            </p>
+          </div>
+          <div className="py-2">
+            <h1 className="sora font-bold text-xl pb-3 uppercase">Social</h1>
+            <p className="flex pb-2">
+              <Social />
+            </p>
+            <a className="text-blue-400" href="mailto:balakumarss10@gmail.com">
+              balakumarss@gmail.com
+            </a>
+          </div>
+        </div>
       </footer>
-      <p className="bg-black text-white text-center text-xs md:text-sm py-2"> &copy;{' '}{date}. This website was developed by Balakumar.</p>
+      <p className="bg-black text-white text-center text-xs md:text-sm py-3">
+        {" "}
+        &copy; {date}. This website was developed by Balakumar.
+      </p>
     </>
   );
 };
